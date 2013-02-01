93209e51b2764373d2ae797af4c88b5161eb5b48
Maurice Fonk <git@naneau.net>
2013-02-01 14:24:40 +0100

    actually using memcacheD, d'oh
    stupid c/p mentality :(

--- travis/before.sh
+++ travis/before.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pecl install memcache
-echo "extension=memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+pecl install memcached
+echo "extension=memcached.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
